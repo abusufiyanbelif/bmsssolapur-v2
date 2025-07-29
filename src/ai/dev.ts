d3dad922af43dbbd16c1be1d575c099d6b238fed
Abusufiyan Belief <abusufiyan.belif@gmail.com>
2025-07-29 20:44:41 +0000

    down/stop all unnecessary service for now until we fix Home page.

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,8 +1,8 @@
 import { config } from 'dotenv';
 config();
 
-import '@/ai/flows/configuration-validator.ts';
-import '@/ai/flows/send-email-flow.ts';
-import '@/ai/flows/send-otp-flow.ts';
-import '@/ai/flows/verify-otp-flow.ts';
+// import '@/ai/flows/configuration-validator.ts';
+// import '@/ai/flows/send-email-flow.ts';
+// import '@/ai/flows/send-otp-flow.ts';
+// import '@/ai/flows/verify-otp-flow.ts';
 import '@/ai/flows/get-inspirational-quotes-flow.ts';
