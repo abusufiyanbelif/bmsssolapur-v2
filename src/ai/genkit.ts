25ddc54cafeaee6da5795dbd3c4364e12c37ca3e
Abusufiyan Belief <abusufiyan.belif@gmail.com>
2025-07-31 14:38:13 +0000

    check again and fix what might be issue in publish let me know. Test app

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,6 @@
 
 import {genkit, type GenkitPlugin} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {firebase} from '@genkit-ai/firebase';
 import {isConfigValid} from '@/services/firebase';
 
 const plugins: GenkitPlugin[] = [googleAI()];
