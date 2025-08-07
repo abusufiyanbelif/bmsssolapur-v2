0129fce204c3faeedd6bf061a8308b141fa51353
Abusufiyan Belief <abusufiyan.belif@gmail.com>
2025-08-07 22:41:15 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/donate/page.tsx
+++ src/app/donate/page.tsx
@@ -24,6 +24,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { useToast } from "@/hooks/use-toast";
 import { useState, useEffect, Suspense, useRef } from "react";
