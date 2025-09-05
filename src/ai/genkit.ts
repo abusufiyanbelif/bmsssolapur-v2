b85719bccbb61ab638660582f61262a0cc355aa1
Abusufiyan Belief <abusufiyan.belif@gmail.com>
2025-09-05 12:27:52 +0000

    [GoogleGenerativeAI Error]: Error fetching from https://generativelangua

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,9 +2,7 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
-const plugins = [googleAI({
-    apiKey: process.env.GEMINI_API_KEY,
-})];
+const plugins = [googleAI()];
 
 export const ai = genkit({
   plugins,
