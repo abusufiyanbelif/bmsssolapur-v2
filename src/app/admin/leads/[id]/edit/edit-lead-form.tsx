76fb6a64ee2bc591a07b4d31a8de421cba690e53
Abusufiyan Belief <abusufiyan.belif@gmail.com>
2025-09-10 23:23:59 +0000

    Due date can be editable

--- src/app/admin/leads/[id]/edit/edit-lead-form.tsx
+++ src/app/admin/leads/[id]/edit/edit-lead-form.tsx
@@ -460,9 +460,6 @@ export function EditLeadForm({ lead, campaigns, users }: EditLeadFormProps) {
                                     mode="single"
                                     selected={field.value}
                                     onSelect={field.onChange}
-                                    disabled={(date) =>
-                                    date < new Date() || date < new Date("1900-01-01")
-                                    }
                                     initialFocus
                                 />
                                 </PopoverContent>
