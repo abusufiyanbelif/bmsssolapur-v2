f0e04513436c0ec2aa8c0de98aa58ba3cb2f1251
Abusufiyan Belief <abusufiyan.belif@gmail.com>
2025-10-12 21:01:27 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Note: You can add any specific Next.js configuration options here in the future.
+};
+
+module.exports = nextConfig;
