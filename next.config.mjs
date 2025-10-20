ac3d4d7e6bf52a12a54dbb6f2f52ca6de0a57239
Abusufiyan Belief <abusufiyan.belif@gmail.com>
2025-10-20 23:20:30 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    config.experiments = { ...config.experiments, asyncWebAssembly: true };
-    return config;
-  },
+    webpack(config) {
+        config.experiments = { ...config.experiments, asyncWebAssembly: true };
+        return config;
+    }
 };
 
 export default nextConfig;
