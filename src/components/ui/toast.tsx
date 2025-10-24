f3593b48d01df64ba07c9b19805c4ef2fd89db4d
Abusufiyan Belief <abusufiyan.belif@gmail.com>
2025-10-24 16:29:18 +0000

    check toast color and fix them accordingly

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -32,7 +32,7 @@ const toastVariants = cva(
       variant: {
         default: "border bg-background text-foreground",
         destructive:
-          "destructive group border-destructive bg-destructive/10 text-destructive-foreground dark:border-destructive [&>svg]:text-destructive",
+          "destructive group border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
         success:
           "success group border-success/50 bg-success/10 text-success-foreground dark:border-success [&>svg]:text-success",
         warning:
@@ -84,7 +84,7 @@ const ToastClose = React.forwardRef<
   <ToastPrimitives.Close
     ref={ref}
     className={cn(
-      "absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-0 transition-opacity hover:text-foreground focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600 group-[.success]:text-green-300 group-[.success]:hover:text-green-50",
+      "absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-0 transition-opacity hover:text-foreground focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.success]:text-green-300 group-[.success]:hover:text-green-50",
       className
     )}
     toast-close=""
